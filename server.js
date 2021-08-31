6b0f88dd99729fee29c6e9f95f7c5dcb3bea39ec
sangeethaNR <sangeethajadhav123@gmail.com>
2021-08-30 23:44:43 -0700

    inquirer prompt added

--- server.js
+++ server.js
@@ -10,3 +10,66 @@ var dbConnection = mysql.createConnection({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME
 });
+//middleware
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}));
+init();
+
+function init()
+{
+    inquirer.prompt([{
+
+        type: "list",
+        message: "What would you like to do?",
+        name: "options",
+        choices: [
+            'View All Employee',
+            'Add Employee',
+            'Update Employee Role',
+            'View All Roles',
+            'Add Role',
+            'View All Departments',
+            'Add Departments'
+        ]
+
+
+    }]).then(function(response){
+        switch(response.options){
+            case 'View All Employee':
+                viewEmployee();
+                break;
+            case 'Add Employee':
+                addEmployee();
+                break;
+           case  'Update Employee Role' :
+                    UpdateEmployeeRole();
+                    break;
+        case  'View All Roles':
+                    ViewRoles();
+                    break;
+                case   'Add Role':
+                    addRoles();
+                    break;
+                
+                case 'View All Departments':
+                    viewDepartment();
+                    break;
+                case    'Add Departments':
+                    addDepartments();
+                    break;
+        }
+    }) ;
+}
+
+function viewEmployee(){
+
+    db.query("SELECT * FROM employee",
+    function (err, results) {
+      if (err) throw err
+      console.table(results)
+      init();
+    })
+};
+
